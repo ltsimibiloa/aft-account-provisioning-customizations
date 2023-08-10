f22a8a4e37dc17516b8dc6a6b75267d38227e1a5
ltsimibilboa <ltsimibiloa@yext.com>
2023-08-10 15:50:45 +0200

    revert reserved_concurrent_executions value to 1 for lambda function of alternate contacts module

--- terraform/modules/aft-alternate-contacts/lambda.tf
+++ terraform/modules/aft-alternate-contacts/lambda.tf
@@ -13,7 +13,7 @@ resource "aws_lambda_function" "aft_alternate_contacts_extract_lambda" {
   tracing_config {
     mode = "Active"
   }
-  reserved_concurrent_executions = 10
+  reserved_concurrent_executions = 1
 }
 
 resource "aws_cloudwatch_log_group" "aft_alternate_contacts_extract_lambda_log" {
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "aft_alternate_contacts_add_lambda" {
   tracing_config {
     mode = "Active"
   }
-  reserved_concurrent_executions = 10
+  reserved_concurrent_executions = 1
 }
 
 resource "aws_cloudwatch_log_group" "aft_alternate_contacts_add_lambda_log" {
@@ -53,7 +53,7 @@ resource "aws_lambda_function" "aft_alternate_contacts_validate_lambda" {
   tracing_config {
     mode = "Active"
   }
-  reserved_concurrent_executions = 10
+  reserved_concurrent_executions = 1
 }
 
 resource "aws_cloudwatch_log_group" "aft_alternate_contacts_validate_lambda_log" {
